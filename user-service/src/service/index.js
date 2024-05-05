677324ab00f345c8ba5aed4d954f39de553ecf44
Rish-2001 <121566349+Rish-2001@users.noreply.github.com>
2024-05-05 16:08:15 +0530

    Token Implemented

--- user-service/src/service/index.js
+++ user-service/src/service/index.js
@@ -1,12 +1,13 @@
 const UserRepository = require('../database/index')
 const { ApiError, ValidationError, AuthorisationError, NotFoundError } = require('../utils/errorClass')
 const RequestResponse = require('../utils/ResponseClass')
-
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 class UserService {
     constructor() {
         this.repo = new UserRepository()
     }
-
+   
     //#region GET ALL USERS : [ADMIN]
     async getAllUsers(){
         try{
@@ -34,7 +35,7 @@ class UserService {
             if(user){
                 throw new ValidationError('User already exists')
             }
-
+            password=null;
             user = await this.repo.addUser({username, password})
             return new RequestResponse(user, "New user added")
 
@@ -45,29 +46,36 @@ class UserService {
     //#endregion
 
     //#region USER LOGIN : [ALL]
-    async loginUser({username, password}){
-        try{
-            if(!username || username == ''){
-                throw new ValidationError('Username not present')
+    
+    async  loginUser({ username, password }) {
+        const secretKey = crypto.randomBytes(32).toString('hex');
+        try {
+            if (!username || username == '') {
+                throw new ValidationError('Username not present');
             }
-            if(!password || password == ''){
-                throw new ValidationError('Password not present')
+            if (!password || password == '') {
+                throw new ValidationError('Password not present');
             }
-
-            const user = await this.repo.findUserByUsername(username)
-
-            if(user){
-                if(user.password === password) return new RequestResponse(user)
-                else throw new AuthorisationError('Incorrect password')
+    
+            const user = await this.repo.findUserByUsername(username);
+    
+            if (user) {
+                if (user.password === password) {
+                    const token = jwt.sign({ username}, secretKey);
+                    user.password = null;
+                    return new RequestResponse({ user, token }); 
+                } else {
+                    throw new AuthorisationError('Incorrect password');
+                }
+            } else {
+                throw new NotFoundError('User with given username does not exist');
             }
-            else{
-                throw new NotFoundError('User with given username does not exist')
+        } catch (err) {
+            if (!(err instanceof ValidationError || err instanceof AuthorisationError || err instanceof NotFoundError)) {
+                throw new ApiError();
+            } else {
+                throw err;
             }
-        }catch(err){
-            if(!((err instanceof ValidationError) || (err instanceof AuthorisationError) || (err instanceof NotFoundError))){
-                throw new ApiError()
-            }
-            else throw err
         }
     }
     //#endregion
