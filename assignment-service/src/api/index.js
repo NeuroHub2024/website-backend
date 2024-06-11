48b9472b5b9f1c4f1171436fbd7c4b0af66566a7
Vaibhav <vaibhav.bajpai1221@gmail.com>
2024-06-12 03:17:40 +0530

    made changes to assignment service

--- assignment-service/src/api/index.js
+++ assignment-service/src/api/index.js
@@ -1,8 +1,52 @@
 const express = require('express')
 const router = express.Router()
+const AssignmentService = require('../service/index')
+const { AuthorisationError } = require('../utils/errorClass')
 
-router.get('/', (req, res)=>{
-    res.send('Assignment service running here')
+const service = new AssignmentService()
+
+//#region GET ALL ASSIGNMENTS : [ADMIN ONLY] : GET /assignment
+router.get('/', async(req, res, next)=>{
+    // res.send('Assignment service running here')
+    try{
+        const role = req.cookies.role
+        if(!role || role != 'Admin') {
+            throw new AuthorisationError('User of this role is not authorized')
+        }
+        console.log(role)
+        const response = await service.getAllAssignments()
+        res.json(response.data)
+    }catch(err){
+        next(err)
+    }
+})
+
+//#region CREATE NEW ASSIGNMENT : [TEACHER, ADMIN] : POST /assignment/createassignment
+router.post('/createassignment/:batchId', async (req, res, next)=> {
+    try{
+        const batchId = req.params.batchId
+        const {title, due, createdBy, body} = req.body
+        const inputs = {
+            batchId, title, due, createdBy, body
+        }
+        const response = await service.createAssignment(inputs)
+        res.json(response.data)
+    }catch(err){
+        next(err)
+    }
+})
+
+//#region SUBMIT RESPONSE TO ASSIGNMENT : [STUDENT, ADMIN] : POST /assignment/createresponse
+router.post('/createresponse/:assignmentId', async(req, res, next)=>{
+    try{
+        const assignmentId = req.params.assignmentId
+        const {studentId, fileUrl} = req.body
+        const inputs = {studentId, fileUrl, assignmentId}
+        const response = await service.createResponse(inputs)
+        res.json(response.data)
+    }catch(err){
+        next(err)
+    }
 })
 
 module.exports = router;
