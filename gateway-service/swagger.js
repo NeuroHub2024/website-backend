1cfca0928cc4489b8d8a2fae559b669079cbdc4e
Vaibhav <vaibhav.bajpai1221@gmail.com>
2024-06-13 21:28:34 +0530

    swagger changes

--- gateway-service/swagger.js
+++ gateway-service/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: 'http://localhost:5000', // Update this with your server URL
+        url: 'https://gateway-mpfy.onrender.com/', // Update this with your server URL
         description: 'Development server',
       },
     ],
