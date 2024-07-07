20f9b9705c0cb5f3c232d77a1e0a44394a3d8925
Vaibhav <vaibhav.bajpai1221@gmail.com>
2024-07-07 13:17:56 +0530

    checking middleware

--- batch-service/src/middlewares/authMiddleware.js
+++ batch-service/src/middlewares/authMiddleware.js
@@ -3,13 +3,13 @@ const axios = require('axios');
 const authUser = async (req, res, next) => {
     console.log('headers : ')
     console.log(req.headers)
-    const token = req.body.token;
-    // const authorization = req.headers['Authorization'];
-    // console.log('authorizaton : ' + authorization)
-    // let token = ''
-    // if(authorization) {
-    //     token = authorization.split(' ')[1]
-    // }
+    // const token = req.body.token;
+    const authorization = req.headers['Authorization'];
+    console.log('authorizaton : ' + authorization)
+    let token = ''
+    if(authorization) {
+        token = authorization.split(' ')[1]
+    }
     console.log('token = ' + token)
 
     if (!token) {
