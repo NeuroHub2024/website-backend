d89665896a819147293f4d736577d1682af5b5bf
ABhay Rawat <abhay892rawat@gmail.com>
2024-07-08 11:41:15 +0530

    adding cors options in assignment-service/server.js file

--- assignment-service/server.js
+++ assignment-service/server.js
@@ -1,24 +1,33 @@
-const express = require('express')
-const app = express()
-const api = require('./src/api/index')
-const cookieParser = require('cookie-parser')
-const cors = require('cors')
-const {PORT} = require('./src/config/index')
+const express = require("express");
+const app = express();
+const api = require("./src/api/index");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const { PORT } = require("./src/config/index");
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
 
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors())
+const corsOptions = {
+  origin: true, // Replace with your frontend domain
+  credentials: true,
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: "content-type, authorization, cookie",
+};
 
-app.use('/', api)
+app.use(cors(corsOptions));
+
+app.use("/", api);
 
 //#region ERROR HANDLING MIDDLEWARE
-app.use((error, req, res, next)=>{
-    const status = error.status || 500
-    const message = error.message
-    return res.status(status).json({message: message})
-})
+app.use((error, req, res, next) => {
+  const status = error.status || 500;
+  const message = error.message;
+  return res.status(status).json({ message: message });
+});
 //#endregion
 
-app.listen(PORT, ()=> console.log(`Assignment service running at http://localhost:${PORT}`))
+app.listen(PORT, () =>
+  console.log(`Assignment service running at http://localhost:${PORT}`)
+);
